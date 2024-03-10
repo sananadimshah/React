de3d601f4b0d0196c50190e08296a50260829763
= <=>
2024-03-10 22:37:03 +0530

    added changes

---
+++ bgChanger/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("pink");
+
+  return (
+    <div>
+      <div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
