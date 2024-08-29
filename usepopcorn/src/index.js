11f9995133a537256f42f64e10e26a8dbac6c8d8
= <=>
2024-08-29 15:31:20 +0530

    added new code

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
+
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./Star.js";
+
+function Test() {
+  const [movieRanting, setMovieRanting] = useState();
+  return (
+    <>
+      <StarRating maxRating={10} userRating={setMovieRanting} />
+      <p>Your gave this movie rating {movieRanting} </p>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+
+    <StarRating
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+
+    <StarRating color="red" size="30" className="test" />
+    <Test />
   </React.StrictMode>
 );
 
